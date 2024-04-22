981eef6ad77ea321ac1d927aa7c2f48672701081
Svitlana Bichurina <itservicedevmy@gmail.com>
2024-04-22 22:09:03 +0200

    adding styles for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import WelcomePage from "./components/WelcomePage";
-import Game from "./components/Game";
-import Ads from "./components/Ads";
-import DetailsAds from "./components/DetailsAds";
-import ResultPage from "./components/ResultPage";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
+import Game from "./components/Game/Game";
+import Ads from "./components/Ads/Ads";
+import DetailsAds from "./components/DetailsAds/DetailsAds";
+import ResultPage from "./components/ResultPage/ResultPage";
 import { useState } from "react";
 
 function App() {
@@ -11,66 +11,6 @@ function App() {
 
   return (
     <>
-      <div className="menu">
-        <button
-          style={{
-            backgroundColor: currentPage === "welcome" ? "#4daa57" : "#f9eccc",
-            color: "#754668",
-          }}
-          onClick={() => {
-            setCurrentPage("welcome");
-          }}
-        >
-          Welcome
-        </button>
-
-        <button
-          style={{
-            backgroundColor: currentPage === "game" ? "#4daa57" : "#f9eccc",
-            color: "#754668",
-          }}
-          onClick={() => {
-            setCurrentPage("game");
-          }}
-        >
-          Game
-        </button>
-        <button
-          style={{
-            backgroundColor: currentPage === "ads" ? "#4daa57" : "#f9eccc",
-            color: "#754668",
-          }}
-          onClick={() => {
-            setCurrentPage("ads");
-          }}
-        >
-          Ads
-        </button>
-        <button
-          style={{
-            backgroundColor: currentPage === "details" ? "#4daa57" : "#f9eccc",
-            color: "#754668",
-          }}
-          onClick={() => {
-            setCurrentPage("details");
-          }}
-        >
-          Details
-        </button>
-        <button
-          style={{
-            backgroundColor: currentPage === "result" ? "#4daa57" : "#f9eccc",
-            color: "#754668",
-          }}
-          onClick={() => {
-            setCurrentPage("result");
-          }}
-        >
-          Result
-        </button>
-      </div>
-      {/* * * * * * * * * * TERNARY OREPATOR * * * * * * * * * * * * * */}
-
       {currentPage === "welcome" ? (
         <WelcomePage setCurrentPage={setCurrentPage} />
       ) : currentPage === "game" ? (
--- src/components/DetailsAds/DetailsAds.jsx
+++ src/components/DetailsAds/DetailsAds.jsx
@@ -3,20 +3,26 @@ import styles from "./DetailsAds.module.css";
 export default function DetailsAds({ setCurrentPage }) {
   return (
     <>
-      <div className={styles.container}>
-        <h2>Details of advertisement</h2>
+      <div
+        className={styles.container}
+        style={{ backgroundColor: "white", border: "solid 5px var(--green2)" }}
+      >
+        <p className={styles.heading}>Details of ads</p>
 
-        <p>
+        <p className={styles.text} style={{ textAlign: "justify" }}>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
           ea eaque at deserunt reprehenderit officiis illum dolorem inventore
           nisi eveniet amet enim possimus, perferendis doloribus tempora
           mollitia distinctio voluptatem rem? <br />
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          ea.
+          ea. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Blanditiis ea eaque at deserunt reprehenderit perferendis doloribus
+          tempora.
         </p>
 
         <a
-          className={styles.detailsLink}
+          className={styles.text}
+          style={{ color: "red" }}
           href="https://www.deutsches-chemie-museum.de/"
         >
           https://www.deutsches-chemie-museum.de/
